373c5d8f61097ee081c7c4f37735ab6189ca9363
cosmdandy-m1 <tkondrashin@icloud.com>
2023-02-27 16:25:57 +0300

    +

--- web/server.py
+++ web/server.py
@@ -3,6 +3,7 @@ import psycopg2
 
 app = Flask(__name__)
 
+
 conn = psycopg2.connect(
     host="postgres",
     port="5432",
@@ -13,16 +14,37 @@ conn = psycopg2.connect(
 cur = conn.cursor()
 
 
+@app.route('/base')
+def base():
+    return render_template("base.html")
+
+
+@app.route('/base_intro')
+def base_intro():
+    return render_template("base_intro.html")
+
+
+@app.route('/base_zero')
+def base_zero():
+    return render_template("base_zero.html")
+
+
 @app.route('/')
-def home():
+@app.route('/intro')
+def intro():
+    return render_template("intro.html")
+
+
+@app.route('/main')
+def main():
     return render_template("main.html")
 
 
-@app.route('/data')
-def database():
-    cur.execute("SELECT * FROM book;")
-    airport = cur.fetchall()
-    return render_template('main.html', airport=airport)
+# @app.route('/data')
+# def database():
+#     cur.execute("SELECT * FROM book;")
+#     airport = cur.fetchall()
+#     return render_template('main.html', airport=airport)
 
 
 if __name__ == "__main__":
