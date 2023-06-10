6542017a4197a93df8fb3cb2e8921ff0cec290a0
Cosm MacBook Air <tkondrashin@icloud.com>
2023-06-10 13:43:02 +0300

    +

--- web/forms.py
+++ web/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, EmailField, TelField, SelectField, StringField, SubmitField
+from wtforms import PasswordField, EmailField, TelField, SelectField, StringField, SubmitField, HiddenField
 from wtforms.validators import DataRequired, EqualTo, Email, Length
 from .models import User, Address, Role, Book, Author, BookAuthor, Genre, BookGenre, Language, BookLanguage, Publisher
 from wtforms_sqlalchemy.fields import QuerySelectField
@@ -45,7 +45,6 @@ class BookSearchForm(FlaskForm):
 
 
 class UserForm(FlaskForm):
-    people_id = StringField('ID пользователя', validators=[DataRequired()])
     people_name = StringField('Имя', validators=[DataRequired()])
     people_surname = StringField('Фамилия', validators=[DataRequired()])
     people_patronymic = StringField('Отчество', validators=[DataRequired()])
@@ -60,14 +59,29 @@ class UserForm(FlaskForm):
     people_role = QuerySelectField(query_factory=lambda: Role.query.order_by(Role.title).all(), get_label="title",
                                    allow_blank=True,
                                    blank_text='Роль')
+    submit = SubmitField('Добавить пользователя')
+
+
+class RoleForm(FlaskForm):
+    title = StringField('Название роли', validators=[DataRequired()])
+    submit = SubmitField('Добавить роль')
 
 
 class BookForm(FlaskForm):
-    book_id = StringField('ID книги', validators=[DataRequired()])
     book_title = StringField('Название книги', validators=[DataRequired()])
     book_desc = StringField('Описание книги', validators=[DataRequired()])
+    book_author = QuerySelectField(query_factory=lambda: Author.query.order_by(Author.name).all()
+                                   , get_label="name", allow_blank=True,
+                                   blank_text='Автор')
+    book_genre = QuerySelectField(query_factory=lambda: Genre.query.order_by(Genre.title).all(), get_label="title",
+                                  allow_blank=True,
+                                  blank_text='Жанр')
+    book_language = QuerySelectField(query_factory=lambda: Language.query.order_by(Language.title).all(),
+                                     get_label="title",
+                                     allow_blank=True,
+                                     blank_text='Язык')
     book_publisher = QuerySelectField(query_factory=lambda: Publisher.query.order_by(Publisher.title).all()
-                                      , get_label="name", allow_blank=True,
+                                      , get_label="title", allow_blank=True,
                                       blank_text='Издательство')
     book_publish_year = StringField('Год издания', validators=[DataRequired()])
     book_publish_month = StringField('Месяц издания', validators=[DataRequired()])
@@ -75,3 +89,41 @@ class BookForm(FlaskForm):
     book_page_num = StringField('Количество страниц', validators=[DataRequired()])
     book_rating = StringField('Рейтинг', validators=[DataRequired()])
     book_num_reviews = StringField('Количество отзывов', validators=[DataRequired()])
+    submit = SubmitField('Добавить книгу')
+
+
+class AuthorForm(FlaskForm):
+    author_name = StringField('Автор', validators=[DataRequired()])
+    author_sex = SelectField(choices=[(None, 'Пол'), (True, 'Мужской'), (False, 'Женский')],
+                             validators=[DataRequired()])
+    submit = SubmitField('Добавить автора')
+
+
+class GenreForm(FlaskForm):
+    genre_title = StringField('Жанр', validators=[DataRequired()])
+    submit = SubmitField('Добавить жанр')
+
+
+class LanguageForm(FlaskForm):
+    lang_title = StringField('Язык', validators=[DataRequired()])
+    submit = SubmitField('Добавить язык')
+
+
+class PublisherForm(FlaskForm):
+    publisher_title = StringField('Название издания', validators=[DataRequired()])
+    publisher_desc = StringField('Описание издания', validators=[DataRequired()])
+    submit = SubmitField('Добавить издателя')
+
+
+# Change
+class ChangeForm(FlaskForm):
+    change_table = HiddenField("Hidden table name")
+    change_id = HiddenField("Hidden table row ID")
+    change = SubmitField("Изменить")
+
+
+# Delete
+class DeleteForm(FlaskForm):
+    delete_table = HiddenField("Hidden table name")
+    delete_id = HiddenField("Hidden table row ID")
+    delete = SubmitField("Удалить")
--- web/main.py
+++ web/main.py
@@ -1,13 +1,15 @@
 import logging
 import random
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, flash, redirect
 from flask_login import login_required
 from sqlalchemy import desc, func, or_
+from werkzeug.security import generate_password_hash
 
 from . import db
-from .forms import BookSearchForm, BookForm, UserForm
-from .models import Book, Author, Genre, Language, Publisher, BookAuthor, BookGenre, BookLanguage
+from .forms import BookSearchForm, BookForm, UserForm, DeleteForm, ChangeForm, RoleForm, AuthorForm, LanguageForm, \
+    GenreForm, PublisherForm
+from .models import Book, Author, Genre, Language, Publisher, BookAuthor, BookGenre, BookLanguage, User, Role, Address
 
 main = Blueprint('main', __name__)
 
@@ -124,58 +126,238 @@ def account():
     return render_template("account.html")
 
 
+def change(table_name, id):
+    if table_name == 'User':
+        User.query.filter_by(id=id).delete()
+    elif table_name == 'Role':
+        Role.query.filter_by(id=id).delete()
+    elif table_name == 'Address':
+        Address.query.filter_by(id=id).delete()
+    elif table_name == 'Book':
+        Book.query.filter_by(id=id).delete()
+    elif table_name == 'Publisher':
+        Publisher.query.filter_by(id=id).delete()
+    # elif table_name == 'Country':
+    #     Country.query.filter_by(id=id).delete()
+    # elif table_name == 'Company':
+    #     Company.query.filter_by(id=id).delete()
+    db.session.commit()
+
+
+def delete(table_name, id):
+    if table_name == 'user':
+        User.query.filter_by(id=id).delete()
+    elif table_name == 'role':
+        Role.query.filter_by(id=id).delete()
+    elif table_name == 'address':
+        Address.query.filter_by(id=id).delete()
+    elif table_name == 'book':
+        Book.query.filter_by(id=id).delete()
+    elif table_name == 'publisher':
+        Publisher.query.filter_by(id=id).delete()
+    elif table_name == 'language':
+        Language.query.filter_by(id=id).delete()
+    elif table_name == 'genre':
+        Genre.query.filter_by(id=id).delete()
+    elif table_name == 'author':
+        Author.query.filter_by(id=id).delete()
+    db.session.commit()
+
+
 @main.route('/admin')
 @login_required
 def admin():
     return render_template("admin.html")
 
 
-@main.route('/admin/users', methods=['POST'])
+@main.route('/admin/users', methods=['GET', 'POST'])
 @login_required
 def admin_users():
+    users_list = db.session.query(
+        User.id, User.name, User.surname, User.patronymic, User.sex, Address.title.label('address'), User.tel_num,
+        User.pass_num, User.email, Role.title.label('role')) \
+        .join(Role, Role.id == User.role) \
+        .join(Address, Address.id == User.address) \
+        .all()
+
     form = UserForm()
-    return render_template("admin_users.html", form=form)
+    if form.validate_on_submit():
+        logging.info("User form validated")
+        new_row = User(
+            name=form.people_name.data,
+            surname=form.people_surname.data,
+            patronymic=form.people_patronymic.data,
+            sex=True if form.people_sex.data == "True" else False,
+            address=form.people_address.data.id,
+            tel_num=form.people_tel_num.data,
+            pass_num=form.people_pass_num.data,
+            email=form.people_email.data,
+            password=generate_password_hash(form.people_password.data, method='sha256'),
+            role=form.people_role.data.id
+        )
+        db.session.add(new_row)
+        db.session.commit()
+        return redirect('/admin/users')
+
+    delete_form = DeleteForm()
+    if delete_form.validate_on_submit():
+        try:
+            delete(delete_form.delete_table.data, delete_form.delete_id.data)
+        except Exception as e:
+            flash(f'{e}')
+        return redirect('/admin/users')
+
+    change_form = ChangeForm()
+    if change_form.validate_on_submit():
+        try:
+            change(change_form.change_table.data, change_form.change_id.data)
+        except Exception as e:
+            flash(f'{e}')
+        return redirect('/admin/users')
+    return render_template("admin_users.html", form=form, users_list=users_list, change_form=change_form,
+                           delete_form=delete_form)
 
 
-@main.route('/admin/roles', methods=['POST'])
+@main.route('/admin/roles', methods=['GET', 'POST'])
 @login_required
 def admin_roles():
-    return render_template("admin_roles.html")
+    roles_list = Role.query.all()
+    form = RoleForm()
+    if form.validate_on_submit():
+        new_row = Role(
+            title=form.title.data,
+        )
+        db.session.add(new_row)
+        db.session.commit()
+        return redirect('/admin/roles')
+
+    delete_form = DeleteForm()
+    if delete_form.validate_on_submit():
+        try:
+            delete(delete_form.delete_table.data, delete_form.delete_id.data)
+        except Exception as e:
+            flash(f'{e}')
+        return redirect('/admin/roles')
+    return render_template("admin_roles.html", form=form, roles_list=roles_list, delete_form=delete_form)
 
 
-@main.route('/admin/books', methods=['POST'])
+@main.route('/admin/books', methods=['GET', 'POST'])
 @login_required
 def admin_books():
+    books_list = db.session.query(
+        Book.id, Book.title, Book.desc, Book.reviews, func.array_agg(func.distinct(Author.name)).label('author'),
+        func.array_agg(func.distinct(Genre.title)).label('genre'), func.array_agg(func.distinct(Language.title)).label('language'), Publisher.title,
+        Book.year, Book.month, Book.day, Book.page_num, Book.rating, Book.page_num, Book.reviews, Publisher.title.label('publisher')) \
+        .join(BookAuthor, BookAuthor.book_book_id == Book.id) \
+        .join(Author, Author.id == BookAuthor.book_author_author_id) \
+        .join(BookGenre, BookGenre.book_id == Book.id) \
+        .join(Genre, Genre.id == BookGenre.genre_id) \
+        .join(BookLanguage, BookLanguage.book_id == Book.id) \
+        .join(Language, Language.id == BookLanguage.lang_id) \
+        .join(Publisher, Book.publisher == Publisher.id) \
+        .group_by(Book.id, Publisher.title, Publisher.desc) \
+        .all()
     form = BookForm()
-    # delete_form = DeleteForm()
-    # if delete_form.validate_on_submit():
-    #     try:
-    #         delete(delete_form.delete_table.data, delete_form.delete_id.data)
-    #     except Exception as e:
-    #         flash(f'{e}')
-    #     return redirect('/admin/PeopleType')
-    return render_template("admin_books.html", form=form)
+
+    delete_form = DeleteForm()
+    if delete_form.validate_on_submit():
+        try:
+            delete(delete_form.delete_table.data, delete_form.delete_id.data)
+        except Exception as e:
+            flash(f'{e}')
+        return redirect('/admin/books')
+    return render_template("admin_books.html", form=form, books_list=books_list, delete_form=delete_form)
 
 
-@main.route('/admin/authors', methods=['POST'])
+@main.route('/admin/authors', methods=['GET', 'POST'])
 @login_required
 def admin_authors():
-    return render_template("admin_authors.html")
+    authors_list = Author.query.all()
+    form = AuthorForm()
+    if form.validate_on_submit():
+        new_row = Author(
+            name=form.author_name.data,
+            sex= True if form.author_sex.data else False
+        )
+        db.session.add(new_row)
+        db.session.commit()
+        return redirect('/admin/authors')
+
+    delete_form = DeleteForm()
+    if delete_form.validate_on_submit():
+        try:
+            delete(delete_form.delete_table.data, delete_form.delete_id.data)
+        except Exception as e:
+            flash(f'{e}')
+        return redirect('/admin/authors')
+    return render_template("admin_authors.html", authors_list=authors_list, form=form, delete_form=delete_form)
 
 
-@main.route('/admin/genres', methods=['POST'])
+@main.route('/admin/genres', methods=['GET', 'POST'])
 @login_required
 def admin_genres():
-    return render_template("admin_genres.html")
+    genre_list = Genre.query.all()
+    form = GenreForm()
+    if form.validate_on_submit():
+        new_row = Genre(
+            title=form.genre_title.data
+        )
+        db.session.add(new_row)
+        db.session.commit()
+        return redirect('/admin/genres')
+
+    delete_form = DeleteForm()
+    if delete_form.validate_on_submit():
+        try:
+            delete(delete_form.delete_table.data, delete_form.delete_id.data)
+        except Exception as e:
+            flash(f'{e}')
+        return redirect('/admin/genres')
+    return render_template("admin_genres.html", genre_list=genre_list, form=form, delete_form=delete_form)
 
 
-@main.route('/admin/languages', methods=['POST'])
+@main.route('/admin/languages', methods=['GET', 'POST'])
 @login_required
 def admin_languages():
-    return render_template("admin_languages.html")
+    language_list = Language.query.all()
+    form = LanguageForm()
+    if form.validate_on_submit():
+        new_row = Language(
+            title=form.lang_title.data
+        )
+        db.session.add(new_row)
+        db.session.commit()
+        return redirect('/admin/languages')
+
+    delete_form = DeleteForm()
+    if delete_form.validate_on_submit():
+        try:
+            delete(delete_form.delete_table.data, delete_form.delete_id.data)
+        except Exception as e:
+            flash(f'{e}')
+        return redirect('/admin/languages')
+    return render_template("admin_languages.html", language_list=language_list, form=form, delete_form=delete_form)
 
 
-@main.route('/admin/publishers', methods=['POST'])
+@main.route('/admin/publishers', methods=['GET', 'POST'])
 @login_required
 def admin_publishers():
-    return render_template("admin_publishers.html")
+    publishers_list = Publisher.query.all()
+    form = PublisherForm()
+    if form.validate_on_submit():
+        new_row = Publisher(
+            title=form.publisher_title.data,
+            desc=form.publisher_desc.data
+        )
+        db.session.add(new_row)
+        db.session.commit()
+        return redirect('/admin/publishers')
+
+    delete_form = DeleteForm()
+    if delete_form.validate_on_submit():
+        try:
+            delete(delete_form.delete_table.data, delete_form.delete_id.data)
+        except Exception as e:
+            flash(f'{e}')
+        return redirect('/admin/publishers')
+    return render_template("admin_publishers.html", publishers_list=publishers_list, form=form, delete_form=delete_form)
